b4b1cf08ee6b94f70839a901ddb5ef1d260031f9
Gopalsatyasaideep <119723488+Gopalsatyasaideep@users.noreply.github.com>
2023-05-23 22:05:35 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        int p=1,s=0,r;
        while(n>0)
        {
            r=n%10;
            p=p*r;
            s=s+r;
            n=n/10;
        }
        if(p==s)
        {
            printf("Spy Number");
        }
        else
        {
            printf("Not Spy Number");
        }
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,35 +1,23 @@
 #include<stdio.h>
 
-int sod(int n)
-{
-    int s=0;
-    while(n>0)
-    {
-        int r = n%10;
-        s = s+r;
-        n/=10;
-    }
-    return s;
-}
-
-int pod(int n)
-{
-    int p =1;
-    while(n>0)
-    {
-        int r = n%10;
-        p*=r;
-        n/=10;
-    }
-    return p;
-}
 int main()
 {
     int n;
     scanf("%d",&n);
-    if(sod(n)==pod(n))
-    printf("Spy Number");
+    int p=1,s=0,r;
+    while(n>0)
+    {
+        r=n%10;
+        p=p*r;
+        s=s+r;
+        n=n/10;
+    }
+    if(p==s)
+    {
+        printf("Spy Number");
+    }
     else
-    printf("Not Spy Number");
-    
+    {
+        printf("Not Spy Number");
+    }
 }

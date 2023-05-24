7062268a6ac60a614b8e1ee19e9c7c62dca55c57
Gopalsatyasaideep <119723488+Gopalsatyasaideep@users.noreply.github.com>
2023-05-24 12:23:06 +0530

    #include<stdio.h>
    int main()
    {
        char c[6]={'0','A','B','C','D','E'};
        int n;
        scanf("%d",&n);
        for(int i=n;i>=1;i--)
        {
            for(int j=1;j<=i;j++)
            {
                printf("%c ",i+64);
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,13 +2,14 @@
 
 int main()
 {
+    char c[6]={'0','A','B','C','D','E'};
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    for(int i=n;i>=1;i--)
     {
         for(int j=1;j<=i;j++)
         {
-            printf("%d",j);
+            printf("%c ",i+64);
         }
         printf("
 ");
